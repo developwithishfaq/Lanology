3f75a00b1fefc4ced1cf0fc87ff899f0e98f65ac
Muhammad Ishfaq <ishfaqcompk6@gmail.com>
2025-04-01 18:17:32 +0500

    done

--- src/main/kotlin/screens/home/HomeScreen.kt
+++ src/main/kotlin/screens/home/HomeScreen.kt
@@ -1,18 +1,11 @@
 package screens.home
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
-import androidx.compose.material.Card
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import domain.models.fakeChats
-import domain.models.fakeServers
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import screens.chat.ChatScreen
 
 @Composable
